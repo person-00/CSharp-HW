d8cabe75400058e1708de0caa121f0ed1c3677f0
person-00 <160495480+person-00@users.noreply.github.com>
2024-09-17 04:36:10 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,17 +6,31 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the length of side A: ");
-            double sideA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the 1st number: ");
+            decimal num1 = Convert.ToDecimal(Console.ReadLine());
 
-            Console.Write("Enter the length of side B: ");
-            double sideB = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the 2nd number: ");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
 
-            double hypotenuse = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+            Console.Write("Enter the 3rd number: ");
+            decimal num3 = Convert.ToDecimal(Console.ReadLine());
 
-            Console.WriteLine($"The length of the hypotenuse is {hypotenuse:F2}");
-            Console.ReadLine();
+            Console.Write("Enter the 4th number: ");
+            decimal num4 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Enter the 5th number: ");
+            decimal num5 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal sum = num1 + num2 + num3 + num4 + num5;
+            decimal avg = sum / 5;
+            Console.WriteLine("The average is " + avg);
+            Console.Read();
         }
     }
 }
+
+
+
+
+
 
