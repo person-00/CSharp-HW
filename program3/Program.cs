1fa488d9a369facf749a8b8757b059b9e8a8be9e
person-00 <160495480+person-00@users.noreply.github.com>
2024-09-17 04:40:49 +0100

    Add files via upload

---
+++ program3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            double input1 = 0;
+            double input2 = 0;
+
+            Console.WriteLine("what is the radius of the cylinder: ");
+            userInput = Console.ReadLine();
+            input1 = double.Parse(userInput);
+
+            Console.WriteLine("what is the height of the cylinder: ");
+            userInput = Console.ReadLine();
+            input2 = double.Parse(userInput);
+
+            double pi = 3.14159;
+            double square = Math.Pow(input1, 2);
+            double volume = (square * input2 * pi);
+            double area = (2 * pi * square + 2 * input1 * input2);
+
+            Console.WriteLine("The volume is: "+volume);
+            Console.WriteLine("The area is: "+area);
+            Console.Read(); 
+
+
+
+        }
+    }
+}
