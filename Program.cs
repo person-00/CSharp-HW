c9d63bbe8a5a1d7c62d0e47b9692096f74951ba7
person-00 <160495480+person-00@users.noreply.github.com>
2024-09-17 04:33:55 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of side A: ");
+            double sideA = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the length of side B: ");
+            double sideB = Convert.ToDouble(Console.ReadLine());
+
+            double hypotenuse = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+
+            Console.WriteLine($"The length of the hypotenuse is {hypotenuse:F2}");
+            Console.ReadLine();
+        }
+    }
+}
+
